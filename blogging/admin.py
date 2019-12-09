986c0bdea2358118e7e69545663d043f98eb30a9
Will Chang <williamchang6@gmail.com>
2019-12-08 18:45:04 -0800

    Submitting lesson07 assignment

--- blogging/admin.py
+++ blogging/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 # a new import
 from blogging.models import Post, Category
 
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
