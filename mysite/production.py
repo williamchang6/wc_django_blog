e25769880eedd8965f6de082a54494629386ab10
Will Chang <williamchang6@gmail.com>
2019-12-18 21:32:07 -0800

    Production settings.

--- mysite/production.py
+++ mysite/production.py
@@ -1,12 +1,12 @@
 import os
 
-import dj_database_url
+import dj-database_url
 
 from .settings import *
 
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
+    'default': dj-database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
     }
 
 DEBUG = False
