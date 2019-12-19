5dd90f4c85fe6656b73dc6133d5a80b41a13ffc0
Will Chang <williamchang6@gmail.com>
2019-12-18 22:32:51 -0800

    Fixing typo in production.py

--- mysite/production.py
+++ mysite/production.py
@@ -1,12 +1,12 @@
 import os
 
-import dj-database_url
+import dj_database_url
 
 from .settings import *
 
 
 DATABASES = {
-    'default': dj-database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
+    'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
     }
 
 DEBUG = False
